c904d54a756c1313ddba09d7d3ba927a986e77d2
khalidhossainbd <khalidhossainbd@gmail.com>
2018-09-05 03:15:59 +0600

    Add New Models

---
+++ CropCalendar/CropCalendar.Models/District.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CropCalendar.Models
+{
+    public class District
+    {
+        [Key]
+        public int DistrictId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string DistrictName { get; set; }
+        [Required]
+        public string DistBbsId { get; set; }
+        public int ZoneAreaId { get; set; }
+        public ZoneArea Zones { get; set; }
+        public int DivisionId { get; set; }
+        public Division Divisions { get; set; }
+    }
+}
---
+++ CropCalendar/CropCalendar.Models/ZoneArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CropCalendar.Models
+{
+    public class ZoneArea
+    {
+        [Key]
+        public int ZoneId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ZoneName { get; set; }
+        [Required]
+        public string ZoneBbsId { get; set; }
+    }
+}
